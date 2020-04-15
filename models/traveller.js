d738b34e2b6f6821932cacd727fd88e610bda214
Tony Tang <teokigi@hotmail.com>
2020-04-15 18:46:42 +0100

    6th pass/failed

--- models/traveller.js
+++ models/traveller.js
@@ -19,7 +19,7 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+    return this.journeys.reduce((journeyTotal,journey)=>journeyTotal + journey.distance,0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
